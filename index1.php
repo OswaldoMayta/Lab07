7646b5ddd7e5e26747198ba1988407b612f1247c
OswaldoMayta <168221171+OswaldoMayta@users.noreply.github.com>
2024-05-01 01:46:41 -0500

    Update index1.php

--- index1.php
+++ index1.php
@@ -3,22 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Usuario</title>
+    <title>Formulario con Escapado de Salida</title>
 </head>
 <body>
-    <h2>Registro de Usuario</h2>
-    <form action="procesar_registro.php" method="post">
+    <h2>Formulario de Registro</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <label for="username">Nombre de usuario:</label>
-        <input type="text" id="username" name="username">
-        <button type="submit">Registrarse</button>
+        <input type="text" id="username" name="username" required>
+        <button type="submit">Enviar</button>
     </form>
 
-     <?php
-     // Output escaping example
-     $username = "<script>alert('XSS attack');</script>";
-     echo htmlspecialchars($username);
-     ?>
+    <?php
+    // Output escaping example
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recuperar el nombre de usuario del formulario
+        $username = $_POST['username'];
+        
+        // Escapar el nombre de usuario para prevenir ataques XSS
+        $escaped_username = htmlspecialchars($username);
+        
+        // Mostrar el nombre de usuario escapado
+        echo "Nombre de usuario ingresado: " . $escaped_username;
+    }
+    ?>
 </body>
 </html>
-
-
